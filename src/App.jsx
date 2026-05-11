7e0956f2dddfb5dac2c181cf4c3a792678c87237
Siliatu1 <147184724+Siliatu1@users.noreply.github.com>
2026-05-11 12:44:43 -0500

    Revert "Merge branch 'james'"
    This reverts commit 90b10cfaaac7383f5b04bfd014bedb008c96cd45, reversing
    changes made to 959cfe155d1999fad55acdade642b540a1412ec2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import LoginForm from "./auth/LoginForm";
-import Dashboard from "./Menu_principal/Dashboard";
-import PanelPrincipal from "./Portal_Lineas_producto/PanelPrincipal";
+import Login from "./auth/Login/Login";
+import MenuPrincipal from "./Menu_principal/componente/MenuPrincipal";
+import Panel from "./Portal_Lineas_producto/pages/Panel";
+import FormHeladeria from "./Portal_Lineas_producto/pages/FormHeladeria";
+import FormRestaurante from "./Portal_Lineas_producto/pages/FromRestaurante";
+import ControlAsistencia from "./Portal_Lineas_producto/pages/ControlAsistencia";
+import FormTodera from "./Portal_Lineas_producto/pages/FormTodera";
 import ProtectedViewRoute from "./auth/components/ProtectedViewRoute";
 
 function App() {
@@ -11,16 +15,32 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* Ruta pública */}
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<Login />} />
 
         {/* Menú principal */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/menu" element={<MenuPrincipal />} />
 
         {/* Portal Líneas de Producto */}
-        <Route path="/lineas-producto" element={<PanelPrincipal />} />
+        <Route path="/lineas-producto" element={<ProtectedViewRoute view="PANEL"><Panel /></ProtectedViewRoute>} />
+        <Route path="/lineas-producto/form-heladeria" element={<ProtectedViewRoute view="FORM_HELADERIA"><FormHeladeria /></ProtectedViewRoute>} />
+        <Route path="/lineas-producto/form-restaurante" element={<ProtectedViewRoute view="FORM_RESTAURANTE"><FormRestaurante /></ProtectedViewRoute>} />
+        <Route path="/lineas-producto/control-asistencia" element={<ProtectedViewRoute view="CONTROL_ASISTENCIA"><ControlAsistencia /></ProtectedViewRoute>} />
+        <Route path="/lineas-producto/form-todera" element={<ProtectedViewRoute view="FORM_TODERA"><FormTodera /></ProtectedViewRoute>} />
 
-        {/* Ruta 404 */}
-        <Route path="*" element={<div>Error 404</div>}/>
+        {/* Ruta protegida (ejemplo) */}
+        <Route
+          path="/dashboard"
+          element={
+            user ? (
+              <div>Bienvenido </div> 
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
--- src/auth/Login/Login.jsx
+++ src/auth/Login/Login.jsx
@@ -26,7 +26,6 @@ const Login = () => {
     }
 
     setLoading(false);
-    
   };
 
   return (
